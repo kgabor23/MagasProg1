a5e883b2eb2ea62a373681767f20b5ac4963f3a3
kgabor23 <55650839+kgabor23@users.noreply.github.com>
2022-05-18 08:32:26 +0200

    Update drill19.cpp

--- drill19.cpp
+++ drill19.cpp
@@ -1,73 +1,117 @@
-#include "std_lib_facilities.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-template<typename T> struct S{
-	S() {}
-	S(T v): val{v} {}
-
-	T& get(); 
+template<typename T>
+struct S {
+	S (T v = 0) : val{v}{} 
+	T& get();
 	const T& get() const;
-
-	void set(T t) { val = t; }
-	S& operator= (const T& t) { val = t; return *this; }
-
-
-	friend istream& operator>> (istream& is, S<T>& t)
-	{
-		is >> t.val;
-		return is;
-	}
-
+	void set(T new_t) {val = new_t;}
+	void operator=(const T& s);
 private:
 	T val;
-    
 };
-  
+
+//getterek
 
 template<typename T>
 T& S<T>::get(){
 	return val;
 }
 
-template<typename T>const T& S<T>::get() const{
+template<typename T>
+const T& S<T>::get() const{
 	return val;
 }
 
-template<typename T>void read_val(T& v){
-	cin >> v;
+template<typename T>
+void S<T>::operator=(const T& s){
+	val = s;
 }
 
-int main()
-	try{
-		S<int> sint {5};
-		sint.set(17);
-		sint=18;
-		cout << sint.get() << endl << "S int: ";
-		read_val(sint);
-		cout << sint.get() << endl;
 
-		S<char> schar {'y'};
-		cout << schar.get() << endl << "S char: ";
-		read_val(schar);
-		cout << schar.get() << endl;
-
-		S<double> sdouble {1.234};
-		cout << sdouble.get() << endl << "S double: ";
-		read_val(sdouble);
-		cout << sdouble.get() << endl;
-	
-		S<string> sstring {"hm"};
-		cout << sstring.get() << endl;
-
-		S<vector<int>> sintvector {vector<int>(9,14)};
-		for(auto i : sintvector.get())
-		cout << i << endl;
-	
-		return 0;
-
+template<typename T>
+void read_val(T& v){
+	cin >> v; 
 }
-	catch(exception& e){
-		cerr << "Exception: " << e.what() << endl;
 
-	}catch(...){
-		cerr << "Exception: " << endl; 
+
+template<typename T>
+std::ostream& operator <<(ostream& os, vector<T>& v){
+	os << "{ ";
+	for(int i = 0; i < v.size(); i++){
+		os << v[i] << (i < v.size()-1 ? ", ": " "); 
 	}
+	os << "}\n";
+	return os;
+}
+
+template<typename T>
+std::istream& operator >>(istream& is, vector<T>& v){
+	char ch = 0;
+	is >> ch;
+	if(ch != '{'){
+		is.unget();
+		return is;
+	}
+	for(T val; is >> val;){
+		v.push_back(val);
+		is >> ch;
+		if (ch != ',') break;
+	}
+
+	return is;
+}
+
+
+int main(){
+	S<int> s;
+	S<int> si {37};
+	S<char> sc {'c'};
+	S<double> sd {3.2};
+	S<std::string> ss {"Hello"};
+	S<std::vector<int>> svi {std::vector<int>{1,1,2,3,5,8}}; 
+
+	cout << "S<int> : " << s.get() << endl
+		 << "S<int> : " << si.get() << endl
+	 	 << "S<char> : " << sc.get() << endl
+	 	 << "S<double> : " << sd.get() << endl
+		 << "S<string> : " << ss.get() << endl;
+		 cout << "S<vector<int>> : ";
+		 for(auto& a : svi.get()){
+		 	cout << a << ' ';
+	}
+	cout << endl;
+
+	sc.set('s');
+	cout << "S<char> : " << sc.get() << endl;
+
+	sd = 42.1;
+	cout << "S<double> : " << sd.get() << endl;
+
+	int ii;
+	read_val(ii);
+	S<int> si2 {ii};
+
+	double dd;
+	read_val(dd);
+	S<double> sd2 {dd};
+
+	string str;
+	read_val(str);
+	S<string> ss2 {str};
+
+	cout << "S<int> : " << si.get() << endl;
+	cout << "S<double> : " << sd2.get() << endl;
+	cout << "S<string> : " << ss2.get() << endl;
+
+	vector<int> vec;
+	read_val(vec);
+	S<vector<int>> svi2 {vec};
+
+	cout << "S<vector<int>> svi2: " << svi2.get() << endl;
+
+
+}
